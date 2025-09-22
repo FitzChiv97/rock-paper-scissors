d572c00399ed9fd733be43e097f4278df17b374a
FitzChiv97 <porter.morgan.97bb@gmail.com>
2025-09-22 22:35:39 +0300

    code refactoring

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -6,7 +6,20 @@ const score = JSON.parse(localStorage.getItem('score')) || {
   ties: 0,
 };
 
-updateScoreElement();   //displays current score on the webpage
+updateScoreElement();   
+//displays current score on the webpage
+
+document.querySelector('.js-rock-btn').addEventListener('click', () => playGame('Rock'));
+document.querySelector('.js-paper-btn').addEventListener('click', () => playGame('Paper'));
+document.querySelector('.js-scissors-btn').addEventListener('click', () => playGame('Scissors'));
+document.querySelector('.js-reset-btn').addEventListener('click', () => resetScore());
+
+
+function updateScoreElement() {
+  document.querySelector('.js-score')
+    .innerHTML = `<p>Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}</p>`;
+}
+
 
 function pickComputerMove() {
   const randomNum = Math.random();
@@ -17,24 +30,26 @@ function pickComputerMove() {
     'Scissors';
 }
 
+
 function playGame(playerMove) {
-  const computerMove = pickComputerMove();      //generates computerMove
+  const computerMove = pickComputerMove();
+  //generates computerMove
+        
   let result = '';
 
-  if (playerMove === 'Rock') {                  //compares player & computer moves
+  if (playerMove === 'Rock') {      
     result = (computerMove === 'Scissors')? 'You win.':
     (computerMove === 'Paper')? 'You lose.': 'Tie.';
-
   } else if (playerMove === 'Paper') {
     result = (computerMove === 'Rock')? 'You win.':
     (computerMove === 'Scissors')? 'You lose.': 'Tie.';
-
   } else if (playerMove === 'Scissors') {
     result = (computerMove === 'Paper')? 'You win.':
     (computerMove === 'Rock')? 'You lose.': 'Tie.';
   };
+  //compares player & computer moves
 
-  switch (result) {                //updates game result object & saves it to localStorage
+  switch (result) {                
     case 'You win.':
       score.wins += 1;
       break;
@@ -49,17 +64,26 @@ function playGame(playerMove) {
   };
   
   localStorage.setItem('score', JSON.stringify(score));
+  //update game result object & save it to localStorage
 
-  updateScoreElement();        //displays updated score on the webpage
+  updateScoreElement();
+  //display updated score on the webpage        
 
-  document.querySelector('.js-result')  //displays game result 
-    .innerHTML = result;
+  document.querySelector('.js-result').innerHTML = `<p>${result}</p>`;
+  //display game result on the webpage
 
-  document.querySelector('.js-moves')  //displays player & computer moves
-    .innerHTML = ` You <img src="images/${playerMove}-emoji.png" class="move-icon"> <img src="images/${computerMove}-emoji.png" class="move-icon"> Computer `;
+  document.querySelector('.js-moves')  
+    .innerHTML = `<p> You <img src="images/${playerMove}-emoji.png" class="move-icon"> <img src="images/${computerMove}-emoji.png" class="move-icon"> Computer </p>`;
+  //display player & computer moves on the webpage
 } 
 
-function updateScoreElement() {
-  document.querySelector('.js-score')
-    .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+
+function resetScore() {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+
+  updateScoreElement();
+  localStorage.setItem('score', JSON.stringify(score));
+  //update score on the wabpage & save it to local storage
 }
